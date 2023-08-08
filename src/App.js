34074f789b82f65422c2d297ff2db91f38614e5b
Billy Cody <william.cody1015@gmail.com>
2023-08-08 16:32:09 -0400

    adding routes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import Contact from './components/contact';
 import Header from './components/header';
 import Portfolio from './components/portfolio';
 import Resume from './components/resume';
-
+import Footer from './components/footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-     
-      
+      <BrowserRouter>
+    <Header></Header> 
+      <Routes>
+      <Route path='/'></Route>
+
+
+
+      </Routes>
+      <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
