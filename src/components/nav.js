2f2499c609e40a79b77abddaec5c93353e9b2c60
Billy Cody <william.cody1015@gmail.com>
2023-08-08 16:41:50 -0400

    added nav links

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Nav() {
     return (
       <div className="nav">
-        <h1>Nav</h1>
+        <ul>
+            <li><Link to="/">About Me</Link></li>
+            <li><Link to="/portfolio">Portfolio</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/resume">Resume</Link></li>
+        </ul>
       </div>
     );
   }
