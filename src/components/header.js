9daec2a8d5835abacb58be00100076ff13892c67
Billy Cody <william.cody1015@gmail.com>
2023-08-08 16:24:25 -0400

    added components

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,7 @@
-// Here we are importing a CSS file as a dependency
-import '../styles/Header.css';
-
 function Header() {
   return (
     <header className="header">
-      <h1>Home</h1>
+      <h1>H</h1>
     </header>
   );
 }
---
+++ src/components/project.js
@@ -0,0 +1,9 @@
+function Project() {
+    return (
+      <div className="project">
+        <h1>Projectt</h1>
+      </div>
+    );
+  }
+  
+  export default Project;
