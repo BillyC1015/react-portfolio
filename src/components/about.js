f42865346bac769cf82de1f4b81a93603b4c04c7
Billy Cody <william.cody1015@gmail.com>
2023-08-17 19:37:55 -0400

    Working on updating about me

--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,11 @@ function About() {
     return (
       <div className="about">
         <h1>About</h1>
+        <h2></h2>
+        <div className="selfie"> 
+        <img src="../img/me_sitting.png" alt="me sitting under a tree in the desert"></img>
+        <h2>I am a newly </h2>
+        </div>
       </div>
     );
   }
